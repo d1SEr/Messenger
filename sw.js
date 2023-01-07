6a7d72f8e0a98feee7b6d42d5cca42f6f570ac83
d1SEr <79686387+d1SEr@users.noreply.github.com>
2023-01-07 15:21:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,34 @@
-'use strict';
-importScripts('sw-toolbox.js'); 
-toolbox.precache(["index.html","style.css", "script.js"]); 
-toolbox.router.get('/img/*', toolbox.cacheFirst); 
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/index.html',
+  '/script.js',
+  '/style.css'
+];
+// событие install
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// событие activate
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// событие fetch
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
